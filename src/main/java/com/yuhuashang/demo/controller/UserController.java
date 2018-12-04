574a5c7957f88e6bf72762eefd457c3dc9951fc6
Edward Wang <yuhuashang@aliyun.com>
2018-12-04 22:11:08 +0800

    modify redis config

---
+++ src/main/java/com/yuhuashang/demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.yuhuashang.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ASUS
+ * @date: 2018/12/4 21:19
+ * @description: 用户的controller
+ */
+@RestController
+public class UserController {
+
+
+}
